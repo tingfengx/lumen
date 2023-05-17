4e1807cf2292aa3ed4d7d24ad1941b89fe6d01e2
tingfengx <tingfeng.xia@outlook.com>
2023-05-16 17:31:26 -0700

    add

--- runner.sh
+++ runner.sh
@@ -3,15 +3,20 @@
 # python train.py --model VGGLIKE_LG
 # python train.py --model VGGLIKE_TI
 
-# python train.py --model SEVGG16_SM
-# python train.py --model SEVGG16_MD
-# python train.py --model SEVGG16_LG
-# python train.py --model SEVGG16_TI
+python train.py --model SEVGG32_SM
+python train.py --model SEVGG32_MD
+python train.py --model SEVGG32_LG
+python train.py --model SEVGG32_TI
 
-python train.py --model SEVGG8_SM
-python train.py --model SEVGG8_MD
-python train.py --model SEVGG8_LG
-python train.py --model SEVGG8_TI
+python train.py --model SEVGG16_SM
+python train.py --model SEVGG16_MD
+python train.py --model SEVGG16_LG
+python train.py --model SEVGG16_TI
+
+# python train.py --model SEVGG8_SM
+# python train.py --model SEVGG8_MD
+# python train.py --model SEVGG8_LG
+# python train.py --model SEVGG8_TI
 
 # python train.py --model SASA_SM
 
